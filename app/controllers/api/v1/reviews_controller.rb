884be11e1a0faca56a95c746474277b518cddc82
Edgar Marques <edgar.eu1@hotmail.com>
2021-03-23 23:16:36 +0000

    Move the App controllers into the Api::V1 namespace

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::ReviewsController < ApplicationController
+  def create
+    product = Product.find(params[:id])
+    @review = product.reviews.new(review_params)
+    @review.save
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:autor, :content, :score)
+  end
 end
