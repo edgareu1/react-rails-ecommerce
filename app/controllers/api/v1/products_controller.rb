7ace56b2348d3a711ebdc33a1b3c2569dc487c2c
Edgar Marques <edgar.eu1@hotmail.com>
2021-03-24 23:06:35 +0000

    Create the Api::V1::Products pages

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.sort
   end
 
   def show
     @product = Product.find(params[:id])
-    @reviews = @product.reviews
-    @review = Review.new
+    @reviews = @product.reviews.sort
   end
 end
