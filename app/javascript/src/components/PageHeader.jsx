b0d7ce8862cbfa6c22f709a6a7d22729481f52d6
Edgar Marques <edgar.eu1@hotmail.com>
2021-03-29 23:15:50 +0100

    Add style to the App and PageHeader component

--- app/javascript/src/components/PageHeader.jsx
+++ app/javascript/src/components/PageHeader.jsx
@@ -1,4 +1,12 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Header = styled.div`
+  text-align: center;
+  color: var(--main-blue);
+  font-size: 5rem;
+  font-weight: bold;
+`
 
 export default class PageHeader extends Component {
   render() {
