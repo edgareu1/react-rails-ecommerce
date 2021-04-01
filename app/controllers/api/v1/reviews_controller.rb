af3536e690e9370dfe4f0acd55bbb23d0da67dee
Edgar Marques <edgar.eu1@hotmail.com>
2021-04-01 19:30:55 +0100

    Solve the Reviews#create action bugs

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ReviewsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
-    product = Product.find(params[:id])
+    product = Product.find(params[:product_id])
     @review = product.reviews.new(review_params)
     @review.save
   end
@@ -8,6 +10,6 @@ class Api::V1::ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:autor, :content, :score)
+    params.require(:review).permit(:author, :content, :score)
   end
 end
