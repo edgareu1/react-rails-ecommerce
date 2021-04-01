256a8cc9f377b76086e94ea7f6aaab8fd768a148
Edgar Marques <edgar.eu1@hotmail.com>
2021-04-01 21:53:29 +0100

    Add validations to the Review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ApplicationRecord
+  validates :author, presence: true
+  validates :content, presence: true
+  validates :score,
+    presence: true,
+    inclusion: 0..5,
+    numericality: { only_integer: true }
+
   belongs_to :product
 end
